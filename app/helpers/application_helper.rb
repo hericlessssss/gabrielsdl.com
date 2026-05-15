c73950e08cfb152b605032cb44feb2f11c7d4c20
chico <hericlessousa@live.com>
2026-05-15 11:24:47 -0300

    style: destacar menu ativo do header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def navigation_link(label, path, active:)
+    link_to label,
+      path,
+      aria: { current: ("page" if active) },
+      class: class_names("nav-link", "nav-link-active": active)
+  end
 end
